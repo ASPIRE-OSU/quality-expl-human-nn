fb7a7a6d030463126df7c48039dfc7056f1f3210
Ada Lamba <lamba.39@osu.edu>
2026-07-01 11:44:22 -0400

    Condense utility scripts in network-expl (fix typo)

--- network-expl/data/utils.py
+++ network-expl/data/utils.py
@@ -695,30 +695,30 @@ def remove_low_prob_f0(datafile='f0.csv', prob_threshold=0.5):
     print('Maximum f0: {:.3f} Hz\n'.format(np.nanmax(df['fmax'].tolist())))
     
     for i, row in tqdm(df.iterrows(), total=len(df)):
-    # clean and read f0 values and voiced probabilities from csv
-    f0 = clean_convert(row['f0'])
-    probs = clean_convert(row['voiced_probs'])    
-    
-    # get indices of frames whose voiced probability is below threshold
-    indices = np.argwhere(probs < prob_threshold).flatten()
-    
-    # clear out those frames' f0 value (set to nan)
-    f0[indices] = np.nan
-    
-    # recalculate f0 stats
-    fmin = np.nanmin(f0)
-    fmax = np.nanmax(f0)
-    favg = np.nanmean(f0)
-    
-    # update df
-    df.at[i, 'fmin'] = fmin
-    df.at[i, 'fmax'] = fmax
-    df.at[i, 'favg'] = favg
-    df.at[i, 'f0'] = f0
-    
-    # checkpoint save
-    if i % 100 == 0:
-        df.to_csv(datafile.split('.csv')[0] + '_threshold.csv', index=False)
+        # clean and read f0 values and voiced probabilities from csv
+        f0 = clean_convert(row['f0'])
+        probs = clean_convert(row['voiced_probs'])    
+        
+        # get indices of frames whose voiced probability is below threshold
+        indices = np.argwhere(probs < prob_threshold).flatten()
+        
+        # clear out those frames' f0 value (set to nan)
+        f0[indices] = np.nan
+        
+        # recalculate f0 stats
+        fmin = np.nanmin(f0)
+        fmax = np.nanmax(f0)
+        favg = np.nanmean(f0)
+        
+        # update df
+        df.at[i, 'fmin'] = fmin
+        df.at[i, 'fmax'] = fmax
+        df.at[i, 'favg'] = favg
+        df.at[i, 'f0'] = f0
+        
+        # checkpoint save
+        if i % 100 == 0:
+            df.to_csv(datafile.split('.csv')[0] + '_threshold.csv', index=False)
     
     # final save 
     df.to_csv(datafile.split('.csv')[0] + '_threshold.csv', index=False)
